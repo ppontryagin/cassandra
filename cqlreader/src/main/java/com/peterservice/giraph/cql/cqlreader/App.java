7928508156b8de6e119bb08c86e4851612e63573
Pavel.Pontryagin <Pavel.Pontryagin@billing.ru>
2014-12-22 16:59:09 +0300

    Реализована  запись и чтение в C* CF из Hadoop.

---
+++ cqlreader/src/main/java/com/peterservice/giraph/cql/cqlreader/App.java
@@ -0,0 +1,13 @@
+package com.peterservice.giraph.cql.cqlreader;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
